1daa60e07d8b1f507a5f23d18c473192a3805966
alcavedon <alcavedon@gmail.com>
2017-02-02 14:40:27 -0500

    .

--- src/MakeChange/Program.cs
+++ src/MakeChange/Program.cs
@@ -9,19 +9,19 @@ namespace MakeChange
     {
         public static void Main(string[] args)
         {
-            public class Change
-            {
-                public int HundredDollarBills { get; }
-                public int FiftyDollarBills { get; }
-                public int TwentyDollarBills { get; }
-                public int TenDollarBills { get; }
-                public int FiveDollarBills { get; }
-                public int DollarBills { get; }
-                public int Quarters { get; }
-                public int Dimes { get; }
-                public int Nickels { get; }
-                public int Pennies { get; }
-            }
+            //public class Change
+            //{
+            //    public int HundredDollarBills { get; }
+            //    public int FiftyDollarBills { get; }
+            //    public int TwentyDollarBills { get; }
+            //    public int TenDollarBills { get; }
+            //    public int FiveDollarBills { get; }
+            //    public int DollarBills { get; }
+            //    public int Quarters { get; }
+            //    public int Dimes { get; }
+            //    public int Nickels { get; }
+            //    public int Pennies { get; }
+            //}
         }
     }
 }
