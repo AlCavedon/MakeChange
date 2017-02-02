285a71eea21a69248c90b3ddf36d39be4442b61f
alcavedon <alcavedon@gmail.com>
2017-02-02 14:19:48 -0500

    now console app

---
+++ src/MakeChange/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeChange
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            public class Change
+            {
+                public int HundredDollarBills { get; }
+                public int FiftyDollarBills { get; }
+                public int TwentyDollarBills { get; }
+                public int TenDollarBills { get; }
+                public int FiveDollarBills { get; }
+                public int DollarBills { get; }
+                public int Quarters { get; }
+                public int Dimes { get; }
+                public int Nickels { get; }
+                public int Pennies { get; }
+            }
+        }
+    }
+}
---
+++ src/MakeChange/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MakeChange")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MakeChange")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7f04fc24-0cf4-4b52-866a-396df37a3a54")]
